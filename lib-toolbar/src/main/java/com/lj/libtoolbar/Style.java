cdcda6dea1c3cc9d9a742eccedc89687a4db0553
liujian <1187502892@qq.com>
2018-09-03 14:36:42 +0800

    本项目将封装一些好的UI工具库

---
+++ lib-toolbar/src/main/java/com/lj/libtoolbar/Style.java
@@ -0,0 +1,12 @@
+package com.lj.libtoolbar;
+
+/**
+ * Created by lj
+ * Description: StatusBar/NavigationBar的样式
+ */
+public enum Style {
+    TRANSPARENT,
+    TRANSLUCENCE,
+    HIDE,
+    DEFAULT
+}
